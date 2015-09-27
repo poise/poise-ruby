941426757ac2ee83bfc0b5ae70830609bb005e3a
Noah Kantrowitz <noah@coderanger.net>
2015-09-27 14:53:28 -0700

    Add a required import.

--- lib/poise_ruby/ruby_providers.rb
+++ lib/poise_ruby/ruby_providers.rb
@@ -14,6 +14,8 @@
 # limitations under the License.
 #
 
+require 'chef/platform/provider_priority_map'
+
 require 'poise_ruby/ruby_providers/chef'
 require 'poise_ruby/ruby_providers/dummy'
 require 'poise_ruby/ruby_providers/scl'
