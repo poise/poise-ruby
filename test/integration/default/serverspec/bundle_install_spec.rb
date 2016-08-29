c3c5b3438f5eec06215cc0e30b19e75e685d7fd8
Noah Kantrowitz <noah@coderanger.net>
2016-08-29 15:24:00 -0700

    Update for new SCL package on EL7.

--- test/integration/default/serverspec/bundle_install_spec.rb
+++ test/integration/default/serverspec/bundle_install_spec.rb
@@ -66,7 +66,14 @@ describe 'bundle_install' do
       it { is_expected.to be_a_file }
     end
 
-    describe command('scl enable rh-ruby22 "/opt/bundle3/bin/rake --version"') do
+    scl_name = case os[:release]
+      when /^6/
+        'rh-ruby22'
+      else
+        'rh-ruby23'
+      end
+
+    describe command("scl enable #{scl_name} \"/opt/bundle3/bin/rake --version\"") do
       its(:exit_status) { is_expected.to eq 0 }
     end
   end
